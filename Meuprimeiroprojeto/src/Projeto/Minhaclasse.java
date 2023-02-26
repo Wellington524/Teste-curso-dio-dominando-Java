e7aaba9c6e64fdbc86c0da36d91954a0bce8f5c4
Wellington Nascimento Alves <wellalves524@gmail.com>
2023-02-26 17:41:31 -0300

    Add files via upload

---
+++ Meuprimeiroprojeto/src/Projeto/Minhaclasse.java
@@ -0,0 +1,16 @@
+package Projeto;
+
+import java.util.Scanner;
+
+public class Minhaclasse {
+
+	public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    System.out.println("Digite um valor!");
+    int valor;
+    valor = teclado.nextInt();
+    System.out.println("valor digitado: " + valor);
+    
+	}
+
+}
